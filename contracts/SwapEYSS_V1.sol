aed15baaee02fc3687fe8e0db6e113c2daf2c321
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2021-05-06 12:40:08 -0400

    Working V1 with multiple tokens

--- contracts/SwapEYSS_V1.sol
+++ contracts/SwapEYSS_V1.sol
@@ -8,9 +8,11 @@ import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 interface IRouter {
 	function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);
 	function WETH() external pure returns (address);
+	function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
 }
 
 interface IERC20 {
+	function name() external view returns (string memory);
 	function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 	function approve(address spender, uint256 amount) external returns (bool);
 	function balanceOf(address account) external view returns (uint256);
@@ -29,7 +31,9 @@ contract SwapEYSS_V1 is OwnableUpgradeable {
 		feeRecipient = _feeRecipient;
 	}
 
-	function swapTokens(address token, uint amountOutMin, uint deadline) public payable {
+	function swapTokens(address[] calldata tokens, uint[] calldata percentages, uint amountOutMin, uint deadline) public payable {
+		require(tokens.length == percentages.length, "Token list and percentages list must be the same length!");
+
 		uint256 fee = 0;
 
 		if (feeRecipient != address(0)) {
@@ -38,13 +42,13 @@ contract SwapEYSS_V1 is OwnableUpgradeable {
 
 		address[] memory path = new address[](2);
 		path[0] = IRouter(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D).WETH();
-		path[1] = token;
 
-		IRouter(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D).swapExactETHForTokens{value: uint256(msg.value).sub(fee)}(amountOutMin, path, msg.sender, deadline);
-
+		for (uint i = 0; i < tokens.length; i++) {
+			require(percentages[i] <= 10000, "Percentage is over 10000");
+			console.log('Swapping', uint256(msg.value).sub(fee).div(10000).mul(uint(percentages[i])), 'wei to:', IERC20(tokens[i]).name());
+			path[1] = tokens[i];
+			IRouter(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D).swapExactETHForTokens{value: uint256(msg.value).sub(fee).div(10000).mul(uint(percentages[i]))}(amountOutMin, path, msg.sender, deadline);
+		}
 		payable(address(feeRecipient)).transfer(fee);
-		//console.log('Done! - Balance:');
-		//console.log(IERC20(token).balanceOf(msg.sender));
 	}
 }
-
--- test/SwapEYSS.js
+++ test/SwapEYSS.js
@@ -32,11 +32,28 @@ describe("EYSS Swap", function () {
 	});
 
 	describe("Transactions", function () {
-		it("Should swap ETH for tokens", async function () {
+		it("Should swap ETH for a token", async function () {
 			let originalBalance = Number(await ethers.utils.formatEther(await ethers.provider.getBalance(owner.address)))
 			let originalBalanceFee = Number(await ethers.utils.formatEther(await ethers.provider.getBalance(await swapInstance.feeRecipient())))
 			await swapInstance.swapTokens(
-				'0xdac17f958d2ee523a2206206994597c13d831ec7', // TetherUSD Address
+				['0xdac17f958d2ee523a2206206994597c13d831ec7'], // TetherUSD Address
+				[10000],
+				1,
+				Number(new Date()) + 24 * 3600,
+				{
+					value: ethers.utils.parseEther("1.0"),
+					gasLimit: 9999999
+				})
+			expect(Number(await ethers.utils.formatEther(await ethers.provider.getBalance(owner.address)))).to.be.below(originalBalance);
+			expect(Number(await ethers.utils.formatEther(await ethers.provider.getBalance(await swapInstance.feeRecipient())))).to.be.above(originalBalanceFee)
+		});
+
+		it("Should swap ETH for more than one token", async function () {
+			let originalBalance = Number(await ethers.utils.formatEther(await ethers.provider.getBalance(owner.address)))
+			let originalBalanceFee = Number(await ethers.utils.formatEther(await ethers.provider.getBalance(await swapInstance.feeRecipient())))
+			await swapInstance.swapTokens(
+				['0xdac17f958d2ee523a2206206994597c13d831ec7', '0x00a8b738E453fFd858a7edf03bcCfe20412f0Eb0', '0x4fabb145d64652a948d72533023f6e7a623c7c53'], // TetherUSD Address
+				[3333, 3333, 3333],
 				1,
 				Number(new Date()) + 24 * 3600,
 				{
